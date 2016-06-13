1bafaf96e00e165ee82638ce37f2c060777a657f
SavaIvanov9 <sava.ivanov.9@gmail.com>
2016-06-14 01:31:58 +0300

    Some Engine and Launcher code added

--- RPG-ConsoleGame/RPG-ConsoleGame/Launcher.cs
+++ RPG-ConsoleGame/RPG-ConsoleGame/Launcher.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPG_ConsoleGame.Engine;
+using RPG_ConsoleGame.UserInterface;
 
 namespace RPG_ConsoleGame
 {
-    class Launcher
+    public class Launcher
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+
+            IRender render = new ConsoleRender();
+            IInputReader reader = new ConsoleInputReader();
+
+            GameEngine engine = new GameEngine(reader, render);
+
+            engine.Run();
         }
     }
 }
