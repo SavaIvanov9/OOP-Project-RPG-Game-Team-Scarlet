33011e456f87d73a60dc1646d9e727e0130ae0ab
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-17 17:14:18 +0300

    Add Building and Shop classes

---
+++ RPG-ConsoleGame/RPG-ConsoleGame/Models/Buildings/Building.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_ConsoleGame.Models.Buildings
+{
+    public abstract class Building
+    {
+        //TO DO:
+        //should inherit from GameObject class
+    }
+}
---
+++ RPG-ConsoleGame/RPG-ConsoleGame/Models/Buildings/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_ConsoleGame.Models.Buildings
+{
+    class Shop
+    {
+        //TO DO:
+        //should inherit from Building class
+    }
+}
