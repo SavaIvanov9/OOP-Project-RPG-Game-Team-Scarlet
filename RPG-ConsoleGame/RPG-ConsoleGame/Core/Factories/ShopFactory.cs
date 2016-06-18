20ee7f695c6cff1db9b7e1bb5fd07dd6686ebae8
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-18 17:40:05 +0300

    Add classes and interfaces for shop creation

---
+++ RPG-ConsoleGame/RPG-ConsoleGame/Core/Factories/ShopFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_ConsoleGame.Core.Factories
+{
+    using Interfaces;
+
+    public class ShopFactory : IShopFactory
+    {
+
+    }
+}
---
+++ RPG-ConsoleGame/RPG-ConsoleGame/Interfaces/IShopable.cs
@@ -0,0 +1,12 @@
+﻿namespace RPG_ConsoleGame.Interfaces
+{
+    using System.Collections;
+    using Items;
+    using System.Collections.Generic;
+
+    public interface IShopable
+    {
+        IList<Item> Inventory { get; set; }
+
+    }
+}
