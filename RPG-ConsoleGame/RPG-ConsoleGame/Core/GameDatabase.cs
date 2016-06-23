a33bcdd417a43c1861193c5e758c86738f734572
Gyokay Ahmed <gyokayakmed@gmail.com>
2016-06-23 11:18:49 +0300

    Add shop functionality to game database

--- RPG-ConsoleGame/RPG-ConsoleGame/Core/GameDatabase.cs
+++ RPG-ConsoleGame/RPG-ConsoleGame/Core/GameDatabase.cs
@@ -6,7 +6,8 @@
     public class GameDatabase : IGameDatabase
     {
         private IList<IPlayer> players = new List<IPlayer>();
-        private IList<IBot> bots = new List<IBot>(); 
+        private IList<IBot> bots = new List<IBot>();
+        private IList<IShop> shop = new List<IShop>();
 
         public IList<IPlayer> Players
         {
@@ -20,6 +21,12 @@
             set { this.bots = value; }
         }
 
+        public IList<IShop> Shop
+        {
+            get { return this.shop; }
+            set { this.shop = value; }
+        }
+
         public void AddBot(IBot bot)
         {
             Bots.Add(bot);
@@ -28,6 +35,11 @@
         public void AddPlayer(IPlayer player)
         {
             Players.Add(player);
+        }
+
+        public void AddShop(IShop shop)
+        {
+            Shop.Add(shop);
         }
     }
 }
--- RPG-ConsoleGame/RPG-ConsoleGame/Interfaces/IGameDatabase.cs
+++ RPG-ConsoleGame/RPG-ConsoleGame/Interfaces/IGameDatabase.cs
@@ -6,8 +6,10 @@
     {
         IList<IPlayer> Players { get; set; }
         IList<IBot> Bots { get; set; }
+        IList<IShop> Shop { get; set; }
 
         void AddPlayer(IPlayer player);
         void AddBot(IBot bot);
+        void AddShop(IShop shop);
     }
 }
