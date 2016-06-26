c520fa09d2299abf6a8e4dda52f8438ea817189e
Aneta Koleva <a.d.koleva@gmail.com>
2016-06-26 09:51:02 +0300

    Added Class ObjectOutOfBoundsException

---
+++ RPG-ConsoleGame/RPG-ConsoleGame/Exceptions/ObjectOutOfBoundsException.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperRpgGame.Exceptions
+{
+    using System;
+
+    public class ObjectOutOfBoundsException : Exception
+    {
+        public ObjectOutOfBoundsException(string message)
+            : base(message)
+        {
+            //used in GameObject Position, but currently is commented there
+        }
+    }
+}
